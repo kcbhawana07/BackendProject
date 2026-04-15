d448e109dd3e70cc53d23665347782520ae0f967
kcbhawana07 <kcbhawana78@gmail.com>
2026-04-15 12:55:44 +0545

    added JWT authentication APIs

--- users/models.py
+++ users/models.py
@@ -1,17 +1,22 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# # Create your models here.
+# class User(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
     
 
+# class User(AbstractUser):
+#     email = models.EmailField(unique=True)
+
+#     USERNAME_FIELD = 'username'
+#     REQUIRED_FIELDS = ['email']
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
