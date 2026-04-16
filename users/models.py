b2da87439e711adc82328824fd2fe2e162c1cc36
kcbhawana07 <kcbhawana78@gmail.com>
2026-04-16 12:18:56 +0545

    Add user authentication APIs (register, login, logout)

--- users/models.py
+++ users/models.py
@@ -1,20 +1,3 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractUser
-# # Create your models here.
-# class User(models.Model):
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField(unique=True)
-#     password = models.CharField(max_length=100)
-
-#     def __str__(self):
-#         return self.name
-    
-
-# class User(AbstractUser):
-#     email = models.EmailField(unique=True)
-
-#     USERNAME_FIELD = 'username'
-#     REQUIRED_FIELDS = ['email']
 from django.contrib.auth.models import AbstractUser #Add extra fields (phone, address, etc.)
 from django.db import models
 class User(AbstractUser):
