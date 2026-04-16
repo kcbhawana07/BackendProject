09b3308c7c6874b519c3a2a77b136fb61313aab6
kcbhawana07 <kcbhawana78@gmail.com>
2026-04-16 13:24:25 +0545

    crud operation for add URL routing for user authentication

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('get/', get_products), # GET all products
+    path('get/', get_products), # GET all products (CRUD OPEARATION)
     path('create/', create_product),# POST create product
     path('update/', update_product),# PUT update product
     path('delete/', delete_product),# DELETE product
