0c751e9f0ed327d7723674b69d4b27c0c1a072b5
kcbhawana07 <kcbhawana78@gmail.com>
2026-04-17 11:51:49 +0545

    crud operation for add URL routing for user authentication and views update for class based form

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import register,logout
+from .views import register,logout,RegisterView,LogoutView
 
 urlpatterns = [
     path('register/', register),
     path('logout/', logout),
+    path('registerclass/', RegisterView.as_view()),
+    path('logoutclass/', LogoutView.as_view()),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view#function based 
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView #for login
 from rest_framework_simplejwt.tokens import RefreshToken #for logouts
+from rest_framework.views import APIView#class based 
+
 
 @api_view(['POST'])
 def register(request):
@@ -39,3 +41,46 @@ def logout(request):
             "success": False,
             "message": "Invalid token"
         })
+    
+
+
+
+
+# ✅ Register API (Class-Based)
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "success": True,
+                "message": "User registered successfully"
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({
+            "success": False,
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+
+# ✅ Logout API (Class-Based)
+class LogoutView(APIView):
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({
+                "success": True,
+                "message": "Logged out successfully"
+            })
+
+        except Exception:
+            return Response({
+                "success": False,
+                "message": "Invalid token"
+            }, status=status.HTTP_400_BAD_REQUEST)
